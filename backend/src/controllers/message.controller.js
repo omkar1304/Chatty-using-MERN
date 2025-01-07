b65a90af2db5c54bfc0e33bba3923d56d3367f3c
Omkar1304 <omkarpedamkar2@gmail.com>
2025-01-07 10:04:44 +0530

    Add: Sidebar to home screen

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
+import User from "../models/user.model.js";
 
 export const getSidebarUsers = async (req, res) => {
   try {
     const loggedInUser = req.user._id;
     const filteredUsers = await User.find({
-      _id: { $ne: mongoose.Types.ObjectId(loggedInUser) },
+      _id: { $ne: loggedInUser },
     });
 
     return res.status(200).json(filteredUsers);
