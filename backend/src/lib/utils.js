36a80e9c3520e6da0ae0dfbfe4b12f779f95d667
Omkarpedamkar13 <omkar.pedamkar@centuryiq.in>
2025-01-19 02:09:52 +0530

    Update: Cookie settings

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
   });
 
   return token;
