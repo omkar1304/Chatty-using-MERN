4d0f7b3b93a7f1c8dbf0f2782fcc74c937488480
Omkarpedamkar13 <omkar.pedamkar@centuryiq.in>
2025-01-19 01:10:55 +0530

    Add: starter response

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import morgan from "morgan"
+import morgan from "morgan";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-import { app, server } from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -15,13 +15,17 @@ const PORT = process.env.PORT;
 //* Middlewares 👇
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.get("/", (req, res) => {
+  res.send("Welcome to chatty backend application!");
+});
 
 //*  Routes 👇
 app.use("/api/auth/", authRoutes);
